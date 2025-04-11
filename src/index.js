fe80dedcf7dbb20aa6b07b306a18330ee867d4b2
EliasSuez <48240167@est.ort.edu.ar>
2025-04-11 09:42:57 -0300

    ej b listo

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express  from "express"; // hacer npm i express
 
 import cors     from "cors";    // hacer npm i cors
 
+import { sumar,restar,multiplicar,dividir } from "./modules/matematica.js";
 
 const app  = express();
 
@@ -39,7 +40,40 @@ app.get('/validarfecha/:ano/:mes/:dia', (req, res) => {
 
 })
 
- 
+app.get('/matematica/sumar', (req, res) => {            
+
+    let resultado = sumar(parseFloat(req.query.n1), parseFloat(req.query.n2))
+    
+    res.status(200).send("El resultado de la operacion es " + resultado)
+
+})
+
+app.get('/matematica/restar', (req, res) => {            
+
+    let resultado = restar(parseFloat(req.query.n1), parseFloat(req.query.n2))
+    
+    res.status(200).send("El resultado de la operacion es " + resultado)
+
+})
+
+app.get('/matematica/multiplicar', (req, res) => {            
+
+    let resultado = multiplicar(parseFloat(req.query.n1), parseFloat(req.query.n2))
+    
+    res.status(200).send("El resultado de la operacion es " + resultado)
+
+})
+
+app.get('/matematica/dividir', (req, res) => {            
+
+    if(parseFloat(req.query.n2) == 0){
+        res.status(400).send("El divisor no puede ser cero")
+    }
+    let resultado = dividir(parseFloat(req.query.n1), parseFloat(req.query.n2))
+    
+    res.status(200).send("El resultado de la operacion es " + resultado)
+
+})
 
 //
 
